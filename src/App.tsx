744845533b8e998277a354769fbe8102a0b9973d
v1cferr <dev.victorferreira@gmail.com>
2026-03-04 17:24:07 -0300

    feat: Implement dark mode and chat sidebar, update branding, and refine layout.

--- src/App.tsx
+++ src/App.tsx
@@ -12,27 +12,36 @@ import Dash from "./pages/Dash";
 import Debts from "./pages/Debts";
 import './App.css'
 
+import { ThemeProvider } from "@/components/theme-provider";
+import { ChatProvider } from "@/components/chat-context";
+import { ChatSidebar } from "@/components/chat-sidebar";
+
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-           <Route path= '/login' element = {<Auth/>} />
-           <Route path= '/dash' element={<Dash/>} />
-           <Route path= '/about' element={<About/>} />
-           <Route path= '/debts' element={<Debts/>} />
-            <Route path= '/resultado' element={<Resultado/>} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ChatProvider>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <ChatSidebar />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+              <Route path='/login' element={<Auth />} />
+              <Route path='/dash' element={<Dash />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/debts' element={<Debts />} />
+              <Route path='/resultado' element={<Resultado />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </ChatProvider>
+  </ThemeProvider>
 );
 
 export default App;
