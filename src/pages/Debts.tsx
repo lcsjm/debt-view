916943330dbe66b9a8224dc683dd7a17050763dc
leka201 <alexiaizabelasantos@hotmail.com>
2026-03-09 15:23:37 -0300

    debts terminado

--- src/pages/Debts.tsx
+++ src/pages/Debts.tsx
@@ -27,84 +27,82 @@ export default function Debts() {
 
   return (
     <>
-      {pToast && (
-        <div className="toast">
-          <p id="toast">{pToast}</p>
-        </div>
-      )}
-
-      <h1>Acompanhe sua dívida</h1>
-      <Link to={"/"}>Voltar</Link>
-
-      <p>
-        aqui estão todos os seus dados para acompanhar o andamento das suas
-        dívidas
-      </p>
-
-      <p>Digite o seu credor</p>
-      <input
-        type="text"
-        value={debts.credor ?? ""}
-        onChange={(e) =>
-          setDebts({ ...debts, credor: e.target.value })
-        }
-      />
-
-      <p>Digite o valor da sua dívida</p>
-      <input
-        type="number"
-        value={debts.valorOriginal ?? ""}
-      
-        onChange={(e) => setDebts({ ...Debts, valorOriginal: parseFloat(e.target.value )})}
-      />
-
-      <p>Digite o valor atual do juros aplicado a sua dívida</p>
-      <input
-        type="number"
-        value={debts.valorAttJuros ?? ""}
-        onChange={(e) =>
-          setDebts((setDebts) => ({...debts, valorAttJuros: parseFloat(e.target.value)}))
-        }
-      />
-
-      <p>Digite a taxa de juros aplicada a sua dívida</p>
-      <input
-        type="number"
-        value={debts.taxaJuros ?? ""}
-        onChange={(e) =>
-          setDebts((setDebts) => ({...debts, taxaJuros: parseFloat(e.target.value)}))}
-      />
-
-      <p>Digite a data de vencimento da sua dívida</p>
-      <input
-        type="date"
-        
-        onChange={(e) =>
-          setDebts((setDebts) => {...debts, dataVenc: e.target.value})}
-      />
-
-      <p>Digite o status da sua dívida</p>
-      <input
-        type="text"
-        value={debts.status ?? ""}
-        onChange={(e) =>
-          setDebts({...debts, status: e.target.value})      
-        }
-      />
-
       <div>
-        <button
-          onClick={() => {
-            if (!debts.credor) {
-              showToast("Informe o credor!");
-              return;
-            }
-            showToast("Seus dados foram salvos");
-          }}
-        >
-          Salvar
-        </button>
+        {pToast && (
+          <div className="toast">
+            <p id="toast">{pToast}</p>
+          </div>
+        )}
+
+        <h1>Acompanhe sua dívida</h1>
+        <Link to={"/"}>Voltar</Link>
+
+        <p>
+          aqui estão todos os seus dados para acompanhar o andamento das suas
+          dívidas
+        </p>
+
+        <p>Digite o seu credor</p>
+        <input
+          type="text"
+          value={debts.credor ?? ""}
+          onChange={(e) => setDebts({ ...debts, credor: e.target.value })}
+        />
+
+        <p>Digite o valor da sua dívida</p>
+        <input
+          type="number"
+          value={debts.valorOriginal ?? ""}
+          onChange={(e) =>
+            setDebts({ ...Debts, valorOriginal: parseFloat(e.target.value) })
+          }
+        />
+
+        <p>Digite o valor atual do juros aplicado a sua dívida</p>
+        <input
+          type="number"
+          value={debts.valorAttJuros ?? ""}
+          onChange={(e) =>
+            setDebts({ ...debts, valorAttJuros: parseFloat(e.target.value) })
+          }
+        />
+
+        <p>Digite a taxa de juros aplicada a sua dívida</p>
+        <input
+          type="number"
+          value={debts.taxaJuros ?? ""}
+          onChange={(e) =>
+            setDebts({ ...debts, taxaJuros: parseFloat(e.target.value) })
+          }
+        />
+
+        <p>Digite a data de vencimento da sua dívida</p>
+        <input
+          type="date"
+          onChange={(e) => setDebts({ ...debts, dataVenc: e.target.value })}
+        />
+
+        <p>Digite o status da sua dívida</p>
+        <input
+          type="text"
+          value={debts.status ?? ""}
+          onChange={(e) => setDebts({ ...debts, status: e.target.value })}
+        />
+
+        <div>
+          <button
+            onClick={() => {
+              if (!debts.credor) {
+                showToast("Informe o credor!");
+                return;
+              }
+              showToast("Seus dados foram salvos");
+            }}
+          >
+            Salvar
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
