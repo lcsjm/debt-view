7ed8f1d5b37682f5fb8d1c5e8778251ab3b4cfb4
v1cferr <dev.victorferreira@gmail.com>
2026-03-09 16:19:49 -0300

    feat: Add `future` prop with `v7_startTransition` and `v7_relativeSplatPath` to `BrowserRouter`.

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,12 @@ const App = () => (
           <Toaster />
           <Sonner />
           <ChatSidebar />
-          <BrowserRouter>
+          <BrowserRouter
+            future={{
+              v7_startTransition: true,
+              v7_relativeSplatPath: true,
+            }}
+          >
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
