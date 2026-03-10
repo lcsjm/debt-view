fceed8aadfe498219b7d1971f1331a22e9a90e46
lcsjm <lucasjm@estudante.ufscar.br>
2026-03-10 17:23:09 -0300

    Aula 10/03 Editar perfil

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,16 @@
+export type Profile = {
+    Name? string
+    cpf? string
+    birth? string
+    cep? string
+    gender? string
+    race? string
+}
+
+export default function Profile (){
+    return (
+        <>
+
+        </>
+    )
+}
