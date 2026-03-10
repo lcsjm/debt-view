f6c53ac7e01ff8700678a0c581aeef16931a493c
guilhermecmello07-coder <guilhermecmello07@gmail.com>
2026-03-10 14:53:39 -0300

    auth funcionando

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -10,7 +10,7 @@ export default function AuthRoute({ children }: { children: ReactNode }) {
   }
 
   if (user) {
-    return <Navigate to="/dash" replace />;
+    return <Navigate to="/resultado" replace />;
   }
 
   return <>{children}</>;
