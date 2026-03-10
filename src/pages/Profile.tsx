ea48cba4d4eb8f7d643ce4cceb1ed8f6fecaba69
lcsjm <lucasjm@estudante.ufscar.br>
2026-03-10 17:32:01 -0300

    auth certo

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,16 +1,64 @@
+import { useState } from "react"
+
 export type Profile = {
-    Name? string
-    cpf? string
-    birth? string
-    cep? string
-    gender? string
-    race? string
+    name?: string,
+    cpf?: string,
+    birth?: string,
+    cep?: string,
+    gender?: string,
+    race?: string,
 }
 
 export default function Profile (){
+// Variável que recebe um tipo própio é um objeto
+    const [prof, setProf] = useState<Profile>({});
     return (
         <>
+            <input 
+                type="text"
+                placeholder="Digite seu nome."
+                value={prof.name}
+                onChange={(e) => setProf({...prof, name: e.target.value})}/>
 
+                <input 
+                type="text"
+                placeholder="Digite seu CPF."
+                value={prof.cpf}
+                onChange={(e) => setProf({...prof, cpf: e.target.value})}/>
+      
+
+                
+            <input 
+                type="date"
+                placeholder="Digite o dia em que você nasceu."
+                value={prof.birth}
+                onChange={(e) => setProf({...prof, birth: e.target.value})}/>
+   
+
+                
+            <input 
+                type="text"
+                placeholder="Digite seu CEP"
+                value={prof.cep}
+                onChange={(e) => setProf({...prof, cep: e.target.value})}/>
+      
+
+                
+            <input 
+                type="text"
+                placeholder="Digite seu gênero"
+                value={prof.gender}
+                onChange={(e) => setProf({...prof, gender: e.target.value})}/>
+      
+
+                
+            <input 
+                type="text"
+                placeholder="Digite sua cor/raça"
+                value={prof.race}
+                onChange={(e) => setProf({...prof, race: e.target.value})}/>
+            
+      
         </>
     )
 }
