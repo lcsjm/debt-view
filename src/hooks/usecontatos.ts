e44f15a3207a770e7afaf034a350d2d42dc29cd2
leka201 <alexiaizabelasantos@hotmail.com>
2026-04-08 16:46:22 -0300

    formulario de fale conosco funcional - por hora nao importa q nao ta enviando no email do usuario

--- src/hooks/usecontatos.ts
+++ src/hooks/usecontatos.ts
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
-import supabase from "utils/supabase";
+import supabase from "../../utils/supabase";
+import { useAuth } from "../context/AuthContext";
 
 export interface ContactData {
   name: string;
@@ -9,10 +10,15 @@ export interface ContactData {
 }
 
 export function useContact() {
+  const { user } = useAuth();
+
   const sendMessage = useMutation({
     mutationFn: async (data: ContactData) => {
+      if (!user) throw new Error("Usuário não autenticado");
+
       const { error } = await supabase.from("contatos").insert([
         {
+          user_id: user.id,
           nome: data.name,
           email: data.email,
           assunto: data.subject,
